19e4b0ac624e2a3441faf14d93275caf05a7cb55
Rishabh-Patel96 <rishabhp007@zohomail.in>
2026-03-12 20:33:23 +0530

    updated

--- sql-projects/01-sql-business-analysis/scripts/dimensions_exploration.sql
+++ sql-projects/01-sql-business-analysis/scripts/dimensions_exploration.sql
@@ -1,11 +1,10 @@
-
 /*
 ===============================================================================
 Dimensions Exploration
 ===============================================================================
 Purpose:
     - To explore the structure of dimension tables.
-	
+
 SQL Functions Used:
     - DISTINCT
     - ORDER BY
@@ -13,15 +12,19 @@ SQL Functions Used:
 */
 
 -- Retrieve a list of unique countries from which customers originate
-SELECT DISTINCT 
-    country 
+SELECT DISTINCT
+    country
 FROM gold.dim_customers
 ORDER BY country;
 
+
 -- Retrieve a list of unique categories, subcategories, and products
-SELECT DISTINCT 
-    category, 
-    subcategory, 
-    product_name 
+SELECT DISTINCT
+    category,
+    subcategory,
+    product_name
 FROM gold.dim_products
-ORDER BY category, subcategory, product_name;
+ORDER BY
+    category,
+    subcategory,
+    product_name;
