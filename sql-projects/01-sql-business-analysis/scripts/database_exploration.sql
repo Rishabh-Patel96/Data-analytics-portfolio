21587ff4fff15619812618f2fea76d11ae558b3c
Rishabh-Patel96 <rishabhp007@zohomail.in>
2026-03-12 20:31:10 +0530

    updated

--- sql-projects/01-sql-business-analysis/scripts/database_exploration.sql
+++ sql-projects/01-sql-business-analysis/scripts/database_exploration.sql
@@ -3,10 +3,11 @@
 Database Exploration
 ===============================================================================
 Purpose:
-    - To explore the structure of the database, including the list of tables and their schemas.
+    - To explore the structure of the database, including the list of tables
+      and their schemas.
     - To inspect the columns and metadata for specific tables.
 
-Table Used:
+Tables Used:
     - INFORMATION_SCHEMA.TABLES
     - INFORMATION_SCHEMA.COLUMNS
 ===============================================================================
@@ -14,17 +15,21 @@ Table Used:
 
 -- Retrieve a list of all tables in the database
 SELECT 
-    TABLE_CATALOG, 
-    TABLE_SCHEMA, 
-    TABLE_NAME, 
-    TABLE_TYPE
-FROM INFORMATION_SCHEMA.TABLES;
+    table_catalog, 
+    table_schema, 
+    table_name, 
+    table_type
+FROM information_schema.tables
+ORDER BY table_schema, table_name;
+
 
 -- Retrieve all columns for a specific table (dim_customers)
 SELECT 
-    COLUMN_NAME, 
-    DATA_TYPE, 
-    IS_NULLABLE, 
-    CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'dim_customers';
+    column_name, 
+    data_type, 
+    is_nullable, 
+    character_maximum_length
+FROM information_schema.columns
+WHERE table_name = 'dim_customers'
+AND table_schema = 'gold'
+ORDER BY ordinal_position;
