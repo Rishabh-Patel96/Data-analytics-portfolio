b0718c592f76e59cdf5428309c845663cbcdfcf9
Rishabh-Patel96 <rishabhp007@zohomail.in>
2026-03-12 20:34:53 +0530

    updated

--- sql-projects/01-sql-business-analysis/scripts/init_database.sql
+++ sql-projects/01-sql-business-analysis/scripts/init_database.sql
@@ -1,115 +1,91 @@
-
 /*
 =============================================================
 Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, this script creates a schema called gold
-	
+    This script creates a new database named 'DataWarehouseAnalytics'.
+    If the database already exists, it is dropped and recreated.
+    It also creates the schema 'gold' and the required tables.
+    
 WARNING:
-    Running this script will drop the entire 'DataWarehouseAnalytics' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
+    Running this script will drop the entire 'DataWarehouseAnalytics'
+    database if it exists.
 */
 
-USE master;
-GO
+-- Drop database if it exists
+DROP DATABASE IF EXISTS "DataWarehouseAnalytics";
 
--- Drop and recreate the 'DataWarehouseAnalytics' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouseAnalytics')
-BEGIN
-    ALTER DATABASE DataWarehouseAnalytics SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouseAnalytics;
-END;
-GO
+-- Create database
+CREATE DATABASE "DataWarehouseAnalytics";
 
--- Create the 'DataWarehouseAnalytics' database
-CREATE DATABASE DataWarehouseAnalytics;
-GO
+-- Connect to the database
+-- (In psql you would run: \c DataWarehouseAnalytics)
 
-USE DataWarehouseAnalytics;
-GO
-
--- Create Schemas
-
+-- Create schema
 CREATE SCHEMA gold;
-GO
+
+-- =============================================================
+-- Create Dimension Tables
+-- =============================================================
 
 CREATE TABLE gold.dim_customers(
-	customer_key int,
-	customer_id int,
-	customer_number nvarchar(50),
-	first_name nvarchar(50),
-	last_name nvarchar(50),
-	country nvarchar(50),
-	marital_status nvarchar(50),
-	gender nvarchar(50),
-	birthdate date,
-	create_date date
+    customer_key INT,
+    customer_id INT,
+    customer_number TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    country TEXT,
+    marital_status TEXT,
+    gender TEXT,
+    birthdate DATE,
+    create_date DATE
 );
-GO
 
 CREATE TABLE gold.dim_products(
-	product_key int ,
-	product_id int ,
-	product_number nvarchar(50) ,
-	product_name nvarchar(50) ,
-	category_id nvarchar(50) ,
-	category nvarchar(50) ,
-	subcategory nvarchar(50) ,
-	maintenance nvarchar(50) ,
-	cost int,
-	product_line nvarchar(50),
-	start_date date 
+    product_key INT,
+    product_id INT,
+    product_number TEXT,
+    product_name TEXT,
+    category_id TEXT,
+    category TEXT,
+    subcategory TEXT,
+    maintenance TEXT,
+    cost INT,
+    product_line TEXT,
+    start_date DATE
 );
-GO
+
+-- =============================================================
+-- Create Fact Table
+-- =============================================================
 
 CREATE TABLE gold.fact_sales(
-	order_number nvarchar(50),
-	product_key int,
-	customer_key int,
-	order_date date,
-	shipping_date date,
-	due_date date,
-	sales_amount int,
-	quantity tinyint,
-	price int 
+    order_number TEXT,
+    product_key INT,
+    customer_key INT,
+    order_date DATE,
+    shipping_date DATE,
+    due_date DATE,
+    sales_amount INT,
+    quantity SMALLINT,
+    price INT
 );
-GO
 
-TRUNCATE TABLE gold.dim_customers;
-GO
+-- =============================================================
+-- Load CSV Data
+-- =============================================================
 
-BULK INSERT gold.dim_customers
-FROM 'C:\Users\Rishabh\Desktop\Data Analyst\SQL\Data-with-Baraa\sql-data-analytics-project\datasets\csv-files\gold.dim_customers.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
+COPY gold.dim_customers
+FROM '/absolute/path/gold.dim_customers.csv'
+DELIMITER ','
+CSV HEADER;
 
-TRUNCATE TABLE gold.dim_products;
-GO
+COPY gold.dim_products
+FROM '/absolute/path/gold.dim_products.csv'
+DELIMITER ','
+CSV HEADER;
 
-BULK INSERT gold.dim_products
-FROM 'C:\Users\Rishabh\Desktop\Data Analyst\SQL\Data-with-Baraa\sql-data-analytics-project\datasets\csv-files\gold.dim_products.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
-
-TRUNCATE TABLE gold.fact_sales;
-GO
-
-BULK INSERT gold.fact_sales
-FROM 'C:\Users\Rishabh\Desktop\Data Analyst\SQL\Data-with-Baraa\sql-data-analytics-project\datasets\csv-files\gold.fact_sales.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
+COPY gold.fact_sales
+FROM '/absolute/path/gold.fact_sales.csv'
+DELIMITER ','
+CSV HEADER;
